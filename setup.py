c07affbfd9bdf092882f2743b630995f10c3d186
Tres Seaver <tseaver@palladion.com>
2016-07-06 08:39:35 -0400

    Replace 'zope.testing.testrunner' -> 'zope.testrunner'.

--- setup.py
+++ setup.py
@@ -54,17 +54,18 @@ setup(name='Products.%s' % NAME,
           ],
       tests_require=[
           'zope.testing >= 3.7.0',
+          'zope.testrunner',
           ],
       extras_require={'docs':['Sphinx',
                              'repoze.sphinx.autointerface',
                              'pkginfo']
           },
-      test_loader='zope.testing.testrunner.eggsupport:SkipLayers',
+      test_loader='zope.testrunner.eggsupport:SkipLayers',
       test_suite='Products.%s.tests' % NAME,
       entry_points="""
       [zope2.initialize]
       Products.%s = Products.%s:initialize
       [distutils.commands]
-      ftest = zope.testing.testrunner.eggsupport:ftest
+      ftest = zope.testrunner.eggsupport:ftest
       """ % (NAME, NAME),
       )
